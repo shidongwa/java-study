90397502d2336f878cb4cc04b43c9175bc41637a
shidongwa <10189795@qq.com>
2023-12-23 16:28:48 +0800

    groupingBy function

--- src/main/java/org/stone/study/core/TestPath.java
+++ src/main/java/org/stone/study/core/TestPath.java
@@ -1,2 +1,18 @@
-package org.stone.study.core;public class TestPath {
+package org.stone.study.core;
+
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class TestPath {
+    public static void main(String[] args) throws URISyntaxException {
+        List<Integer> myIntegers = Arrays.asList(1, 1, 2, 3, 4, 2);
+        Map<Integer, Long> myIntegerMap = myIntegers.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        long sum = myIntegerMap.values().stream().mapToLong(v -> v/2).sum();
+        System.out.println("sum:" + sum);
+
+    }
 }
